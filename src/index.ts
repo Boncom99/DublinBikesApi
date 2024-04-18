321518dede267005ae815d4a822a240b49f883bc
Boncom99 <josepbmoya@gmail.com>
2024-04-18 21:17:13 +0200

    eslint + prettier

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,34 @@
-import express from 'express'
+import express from 'express';
 //fetch
-import fetch from 'node-fetch'
-import {getSchema} from "./computeSchema";
-const app = express()
-const port = 3000
-let schema= {}
+import fetch from 'node-fetch';
+import { getSchema } from './computeSchema';
+const app = express();
+const port = 3000;
+let schema = {};
 
-fetch('https://app-media.noloco.app/noloco/dublin-bikes.json').then(res => res.json()).then(json => {
-    console.log(json)
-    if(!Array.isArray(json)){
-       throw new Error('json is not an array')
+fetch('https://app-media.noloco.app/noloco/dublin-bikes.json')
+  .then((res) => res.json())
+  .then((json) => {
+    console.log(json);
+    if (!Array.isArray(json)) {
+      throw new Error('json is not an array');
     }
-    schema=getSchema(json)
-    console.log(schema)
-})
+    schema = getSchema(json);
+    console.log(schema);
+  });
 
 app.get('/schema', (req, res) => {
-    res.send(schema)
-})
+  res.send(schema);
+});
 app.post('/data', (req, res) => {
-    fetch('https://app-media.noloco.app/noloco/dublin-bikes.json').then(res => res.json()).then(json => {
-        //filter the json
-        //return the filtered json
-        res.send(json)
-    })
-})
+  fetch('https://app-media.noloco.app/noloco/dublin-bikes.json')
+    .then((res) => res.json())
+    .then((json) => {
+      //filter the json
+      //return the filtered json
+      res.send(json);
+    });
+});
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
